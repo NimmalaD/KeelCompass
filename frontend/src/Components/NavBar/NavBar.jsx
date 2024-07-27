c29dd1ae7931e2efb581218e82c6e9fd4c70e8bb
Dharma Thanishq Nimmala <nimmala.d@northeastern.edu>
2024-07-27 00:39:39 -0400

    created navbar

--- frontend/src/Components/NavBar/NavBar.jsx
+++ frontend/src/Components/NavBar/NavBar.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import "./NavBar.css";
 
 const NavBar = () => {
   return (
-    <h1 className='text-x1 font-bold text-red-500'>NavBar</h1>
-  )
-}
+    <nav className="navbar bg-white p-4 text-black shadow-lg font-sans">
+      <div className="container mx-auto flex justify-between items-center">
+        <a href="/" className="text-black text-2xl font-bold">
+          <img src="/images/keel-works-logo.png" alt="keelworks" className="keel-works-logo" />
+        </a>
+        <input
+          type="text"
+          placeholder="What do you want to know?"
+          className="border rounded-full py-2 px-4 focus:outline-none focus:border-blue-500 search-bar"
+        />
+        <ul className="flex space-x-4">
+          <li className="flex items-center">
+          <a
+              className="question-mark"
+              href="#"
+            >
+              ?
+            </a>
+            
+          </li>
+          <li>
+            <a
+              className="inline-block text-center w-28 border border-black rounded-full py-2 px-4 bg-transparent text-black"
+              href="#"
+            >
+              Log In
+            </a>
+          </li>
+          <li>
+            <a
+              className="inline-block text-center w-28 border custom-blue rounded-full py-2 px-4 bg-blue-500 text-white"
+              href="#"
+            >
+              Sign Up
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
